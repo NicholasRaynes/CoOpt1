387dcb7274933f37933214740dcff3fedbe15935
NicholasRaynes <nicholasraynesmb@gmail.com>
2023-09-11 12:49:24 -0500

    Created first basic toast with button

--- app/src/main/java/com/zan/coopt1/MainActivity.kt
+++ app/src/main/java/com/zan/coopt1/MainActivity.kt
@@ -1,46 +1,55 @@
 package com.zan.coopt1
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.zan.coopt1.ui.theme.CoOpt1Theme
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ShowToastButton() {
+    val context = LocalContext.current
+
+    var showToast by remember { mutableStateOf(false) }
+
+    if (showToast) {
+        Toast.makeText(context, "Button Clicked!", Toast.LENGTH_SHORT).show()
+        showToast = false
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { showToast = true },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Show Toast")
+        }
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CoOpt1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            ShowToastButton()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CoOpt1Theme {
-        Greeting("Android")
     }
 }
