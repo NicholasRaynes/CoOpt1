0ef080516c5c597c569de364601c04841d8e32e2
AmandaSchepp <aschepp@academic.rrc.ca>
2023-09-13 14:17:57 -0500

    f and l name user input

--- app/src/main/java/com/zan/coopt1/MainActivity.kt
+++ app/src/main/java/com/zan/coopt1/MainActivity.kt
@@ -9,38 +9,54 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShowToastButton() {
     val context = LocalContext.current
 
     var showToast by remember { mutableStateOf(false) }
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
 
     if (showToast) {
-        Toast.makeText(context, "Button Clicked!", Toast.LENGTH_SHORT).show()
+        val message = "Hello, $firstName $lastName"
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
         showToast = false
     }
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        OutlinedTextField(
+            value = firstName,
+            onValueChange = { newValue -> firstName = newValue },
+            label = { Text(text = "Enter your first name") },
+            modifier = Modifier.padding(16.dp)
+        )
+
+        OutlinedTextField(
+            value = lastName,
+            onValueChange = { newValue -> lastName = newValue },
+            label = { Text(text = "Enter your last name") },
+            modifier = Modifier.padding(16.dp)
+        )
+
         Button(
             onClick = { showToast = true },
             modifier = Modifier.padding(16.dp)
         ) {
-            Text(text = "Show Toast")
+            Text(text = "Done")
         }
     }
 }
